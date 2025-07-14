8a34445673601a2a8e8a1603525a915864dfe119
Abhishek Sharma <geekysharma31@gmail.com>
2025-07-14 12:56:51 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Therapy",
-  description: "Best therapy app",
+  title: "Dr Serena Blake",
+  description: "Personal website for a therapist",
 };
 
 export default function RootLayout({
